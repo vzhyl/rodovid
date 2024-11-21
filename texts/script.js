02693c61ae95fdbbb45391bc3186fab7513ca32b
vzhyl <vzhylochkin@gmail.com>
2024-11-21 19:35:55 +0200

    Mobile styles applied to Texts page

--- texts/script.js
+++ texts/script.js
@@ -5,19 +5,18 @@ const outputObj = document.getElementById("textOutput");
 const backBtn = document.getElementById("backBtn");
 
 var fileArray;
-fetch('data/text_database.json')
+fetch("data/text_database.json")
     .then((res) => {
         return res.json();
-    }).then((data) => {
+    })
+    .then((data) => {
         fileArray = data;
-        outputList(data, "")
+        outputList(data, "");
     });
-
-
 
 searchBar.addEventListener("input", () => {
     outputList(fileArray, searchBar.value);
-})
+});
 
 function outputList(data, matchStr) {
     listObj.innerHTML = "";
@@ -32,72 +31,71 @@ function outputList(data, matchStr) {
             fileLink.innerHTML = el;
             eventList.push([fileLink, `data/${data[el]["Файл"]}`]);
 
-
             newListElement.append(fileLink);
 
             var containerElement = document.createElement("div");
-            containerElement.innerHTML += `<p><span>${data[el]["Дата створення"]}</span> ${data[el]["Автор"]}</p>`
+            containerElement.innerHTML += `<p><span>${data[el]["Дата створення"]}</span> ${data[el]["Автор"]}</p>`;
 
             var markersUl = document.createElement("ul");
 
             for (marker in data[el]["Маркери"]) {
                 var markersLi = document.createElement("li");
-                markersLi.innerHTML = data[el]["Маркери"][marker]
-                markersUl.append(markersLi)
+                markersLi.innerHTML = data[el]["Маркери"][marker];
+                markersUl.append(markersLi);
             }
 
             containerElement.append(markersUl);
             newListElement.append(containerElement);
 
             listObj.appendChild(newListElement);
-
         }
     }
     eventList.forEach(([obj, link]) => {
         obj.addEventListener("click", function() {
-            scrollToTop(goToText, link)
-        })
-    })
+            scrollToTop(goToText, link);
+        });
+    });
 }
+let mediaMatch = window.matchMedia("(min-width: 1100px)");
 
 function goToText(textLink) {
-    searchObj.style.left = "-100%";
-    outputObj.style.left = "25%";
-    backBtn.style.left = "50px";
-
     fetch(textLink)
         .then((res) => res.text())
         .then((mdText) => {
             var converter = new showdown.Converter(),
                 text = mdText,
                 html = converter.makeHtml(text);
-            outputObj.innerHTML = html
+            outputObj.innerHTML = html;
 
-            imgSettings()
+            imgSettings();
         })
         .catch((e) => console.error(e));
+
+    document.body.classList.toggle("open");
 }
 
 function goBackBtn() {
-    searchObj.style.left = "15%";
-    outputObj.style.left = "100%";
-    backBtn.style.left = "-25%";
+
+    document.body.classList.toggle("open");
+    setTimeout(function() {
+        outputObj.innerHTML = ""
+    }, 750)
 }
 
 function scrollToTop() {
     let func = arguments[0];
     let funcArgs = [];
     for (let argN = 0; argN < arguments.length - 1; argN++) {
-        funcArgs.push(arguments[argN + 1])
+        funcArgs.push(arguments[argN + 1]);
     }
 
     globalThis.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     if (window.scrollY == 0) {
-        func(funcArgs)
+        func(funcArgs);
     } else {
         setTimeout(function() {
-            func(funcArgs)
-        }, 750)
+            func(funcArgs);
+        }, 750);
     }
 }
 
@@ -105,8 +103,8 @@ function imgSettings() {
     var allImgs = document.getElementsByTagName("img");
     for (let i = 0; i < allImgs.length; i++) {
         allImgs[i].addEventListener("click", function() {
-            window.open(allImgs[i].src)
-        })
-        allImgs[i].title = "Відкрити в повноекранному режимі"
+            window.open(allImgs[i].src);
+        });
+        allImgs[i].title = "Відкрити в повноекранному режимі";
     }
 }
